5a9517237a7730504bc90f8ab38b7865ddc89ab9
Alex Anghel <alexisky@gmail.com>
2022-11-11 22:25:15 +0200

    add antMatcher with star/s

--- src/main/java/com/example/springsecuritydg/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydg/SecurityConfiguration.java
@@ -28,9 +28,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/index.html").permitAll()
-                .antMatchers("/profile/index").authenticated()
-                .antMatchers("/admin/index").hasRole("ADMIN")
-                .antMatchers("/management/index").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
                 .and()
                 .httpBasic();
     }
