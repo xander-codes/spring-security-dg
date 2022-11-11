34d0f0df71dee45a4943349ef38d2905c576cc52
Alex Anghel <alexisky@gmail.com>
2022-11-11 21:33:03 +0200

    initial security

---
+++ src/main/java/com/example/springsecuritydg/SpringSecurityDgApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritydg/SpringSecurityDgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
